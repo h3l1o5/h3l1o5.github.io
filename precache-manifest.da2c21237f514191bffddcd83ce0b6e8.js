83e9d90d1775fb05b533d0e9677993872471da49
martysun <martysun@synology.com>
2020-06-03 14:19:51 +0800

    yo

---
+++ precache-manifest.da2c21237f514191bffddcd83ce0b6e8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b85a9af0014aa18d68e7eb0928eaece6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6005c135f4701025a8b6",
+    "url": "/static/css/main.660b756e.chunk.css"
+  },
+  {
+    "revision": "c202c43ddf6d3e6c4934",
+    "url": "/static/js/2.b0f5f975.chunk.js"
+  },
+  {
+    "revision": "396656086d233a6f84f93b6161ecfda3",
+    "url": "/static/js/2.b0f5f975.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6005c135f4701025a8b6",
+    "url": "/static/js/main.93b62687.chunk.js"
+  },
+  {
+    "revision": "eb82e99ccc090fefe35f",
+    "url": "/static/js/runtime-main.c186c3f7.js"
+  }
+]);
